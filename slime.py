5cc16c2952a5b2b9a440c836c7dbcef633ddbae9
William Peiter <williampeiter@hotmail.com>
2022-10-21 22:46:04 -0600

    try adding trail detection

--- slime.py
+++ slime.py
@@ -7,20 +7,22 @@ class Simulation:
     # delay between frames in ms
     DELAY: int = 1
     RENDER_SPEED: int = 1
-    SPEED: int = 8
+    SPEED: int = 5
     MAP_SIZE: int = 500
     AGENTS_NUM: int = 50
     CIRCLE_SIZE: float = 0.4
     SIZE: int = 3
-    TRAIL_STRENGTH: float = 50
+    TRAIL_STRENGTH: float = 25
+    AGENT_FOV: float = np.pi / 6
+    VISION_DISTANCE: int = 2 * SPEED
 
     # How heavy the blur is on the map
     # Which is used to spread out trails
-    BLUR_STRENTH: int = 15
+    BLUR_STRENTH: int = 11
 
     # How fast trails fade to 0
     # Trails are multipled by this value every step
-    DISSIPATE_TARGET: int = AGENTS_NUM*700
+    DISSIPATE_TARGET: int = AGENTS_NUM * 400
 
     def __init__(self) -> None:
         self.tick = 0
@@ -58,17 +60,37 @@ class Simulation:
         y = radius * np.sqrt(t) * np.sin(2 * np.pi * u) + self.MAP_SIZE / 2
         r = np.random.rand(self.AGENTS_NUM) * 2 * np.pi
 
-        self.agents = (
-            np.column_stack(
-                [
-                    x,
-                    y,
-                    r,
-                ]
-            )
+        self.agents = np.column_stack(
+            [
+                x,
+                y,
+                r,
+            ]
         )
 
     def update_agents(self) -> None:
+
+        # rotate agent direction (vector)
+        np.apply_along_axis(self.rotate, 0, self.agents[:, 2])
+
+        # rad_straight = self.agents[:, 2]
+        # straight = self.get_vision(
+        #     np.sin(self.agents[:, 2]),
+        #     np.cos(self.agents[:, 2]),
+        # )
+
+        # rad_right = rad_straight - self.AGENT_FOV
+        # right = self.get_vision(
+        #     np.sin(rad_right),
+        #     np.cos(rad_right),
+        # )
+
+        # rad_left = rad_straight + self.AGENT_FOV
+        # left = self.get_vision(
+        #     np.sin(rad_left),
+        #     np.cos(rad_left),
+        # )
+
         # update agents position
         self.agents[:, 0] += self.SPEED * np.sin(self.agents[:, 2])
         self.agents[:, 1] += self.SPEED * np.cos(self.agents[:, 2])
@@ -78,7 +100,8 @@ class Simulation:
 
         # keep agents in bounds
         self.agents[:, :2][~in_bounds] = np.clip(
-            self.agents[:, :2][~in_bounds], 0, self.MAP_SIZE - 1)
+            self.agents[:, :2][~in_bounds], 0, self.MAP_SIZE - 1
+        )
 
         # update agents directions (radians)
         # bouncing off horizontal walls means inverting the radians
@@ -86,11 +109,40 @@ class Simulation:
         self.agents[:, 2][~in_bounds.all(axis=1)] *= -1
         self.agents[:, 2][~in_bounds[:, 1]] += np.pi
 
+    def rotate(
+        self,
+        rad: np.float64,
+    ) -> np.float64:
+
+        # straight
+        h = self.VISION_DISTANCE * np.sin(rad)
+        w = self.VISION_DISTANCE * np.cos(rad)
+        trail_sum = self.trails_map[h - 3: h + 3, w - 3: w + 3].sum()
+        result = rad
+
+        # left
+        rad_left = rad + self.AGENT_FOV
+        h = self.VISION_DISTANCE * np.sin(rad_left)
+        w = self.VISION_DISTANCE * np.cos(rad_left)
+        val = self.trails_map[h - 3: h + 3, w - 3: w + 3].sum()
+        if val > trail_sum:
+            trail_sum = val
+            result = rad_left
+
+        # right
+        rad_right = rad - self.AGENT_FOV
+        h = self.VISION_DISTANCE * np.sin(rad_right)
+        w = self.VISION_DISTANCE * np.cos(rad_right)
+        trail_sum = self.trails_map[h - 3: h + 3, w - 3: w + 3].sum()
+        if val > trail_sum:
+            trail_sum = val
+            result = rad_right
+
+        return result
+
     def lay_trails(self) -> None:
         h, w = self.agents[:, :2].astype(int).T
-        self.trails_map[
-            h, w
-        ] += self.TRAIL_STRENGTH
+        self.trails_map[h, w] += self.TRAIL_STRENGTH
 
     def dissipate_trails(self):
         self.trails_map = cv.GaussianBlur(
@@ -101,7 +153,6 @@ class Simulation:
 
         while self.trails_map.sum() > self.DISSIPATE_TARGET:
             self.trails_map *= 0.95
-
 
     def draw_map(self) -> None:
         self.frame = np.zeros((self.MAP_SIZE, self.MAP_SIZE))
