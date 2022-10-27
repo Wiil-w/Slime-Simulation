02880348a2ab190d601419355ce6834b5584710d
William Peiter <williampeiter@hotmail.com>
2022-10-27 10:04:59 -0300

    change atributes to snake_case

--- slime.py
+++ slime.py
@@ -5,25 +5,25 @@ import cv2 as cv
 class Simulation:
 
     # delay between frames in ms
-    DELAY: int = 1
-    RENDER_SPEED: int = 1
-    SPEED: int = 1
-    MAP_SIZE: int = 500
-    AGENTS_NUM: int = 100
-    CIRCLE_SIZE: float = 0.4
-    TRAIL_STRENGTH: float = 15
-    AGENT_FOV: float = np.pi / 12
-    VISION_DISTANCE: int = SPEED * 2
+    delay: int = 1
+    render_speed: int = 1
+    speed: int = 1
+    map_size: int = 500
+    agents_num: int = 100
+    circle_size: float = 0.4
+    trail_strength: float = 15
+    agent_fov: float = np.pi / 12
+    vision_distance: int = speed * 2
 
     # How heavy the blur is on the map
     # Which is used to spread out trails
-    BLUR_STRENTH: int = 5
-    BLUR_DELAY: int = 3
-    DISSIPATION_RATE: float = 10
+    blur_strenth: int = 5
+    blur_delay: int = 3
+    dissipation_rate: float = 10
 
     def __init__(self) -> None:
         self.tick = 0
-        self.trails_map = np.zeros((self.MAP_SIZE, self.MAP_SIZE))
+        self.trails_map = np.zeros((self.map_size, self.map_size))
         self.create_agents()
 
     def update(self) -> None:
@@ -33,10 +33,10 @@ class Simulation:
             self.update_simulation()
             self.draw_map()
 
-            if not (self.tick % self.RENDER_SPEED):
+            if not (self.tick % self.render_speed):
                 cv.imshow("Simulation", self.frame)
 
-            k = cv.waitKey(self.DELAY)
+            k = cv.waitKey(self.delay)
             if k == 113:
                 break
 
@@ -48,14 +48,14 @@ class Simulation:
     def create_agents(self) -> None:
         """create_agents creates a collection of agents in a circle"""
 
-        radius = int(self.CIRCLE_SIZE * self.MAP_SIZE)
+        radius = int(self.circle_size * self.map_size)
 
-        t = np.random.uniform(0, 1, size=self.AGENTS_NUM)
-        u = np.random.uniform(0, 1, size=self.AGENTS_NUM)
+        t = np.random.uniform(0, 1, size=self.agents_num)
+        u = np.random.uniform(0, 1, size=self.agents_num)
 
-        x = radius * np.sqrt(t) * np.cos(2 * np.pi * u) + self.MAP_SIZE / 2
-        y = radius * np.sqrt(t) * np.sin(2 * np.pi * u) + self.MAP_SIZE / 2
-        r = np.random.rand(self.AGENTS_NUM) * 2 * np.pi
+        x = radius * np.sqrt(t) * np.cos(2 * np.pi * u) + self.map_size / 2
+        y = radius * np.sqrt(t) * np.sin(2 * np.pi * u) + self.map_size / 2
+        r = np.random.rand(self.agents_num) * 2 * np.pi
 
         self.agents = np.column_stack(
             [
@@ -71,15 +71,15 @@ class Simulation:
         self.rotate()
 
         # update agents position
-        self.agents[:, 0] += self.SPEED * np.sin(self.agents[:, 2])
-        self.agents[:, 1] += self.SPEED * np.cos(self.agents[:, 2])
+        self.agents[:, 0] += self.speed * np.sin(self.agents[:, 2])
+        self.agents[:, 1] += self.speed * np.cos(self.agents[:, 2])
 
         in_bounds = (0 <= self.agents[:, :2]) & (
-            self.agents[:, :2] < self.MAP_SIZE)
+            self.agents[:, :2] < self.map_size)
 
         # keep agents in bounds
         self.agents[:, :2][~in_bounds] = np.clip(
-            self.agents[:, :2][~in_bounds], 0, self.MAP_SIZE - 1
+            self.agents[:, :2][~in_bounds], 0, self.map_size - 1
         )
 
         # update agents directions (radians)
@@ -93,13 +93,13 @@ class Simulation:
         trail_sum = self.calc_rotation(self.agents[:, 2])
         result = self.agents[:, 2]
 
-        rad_left = self.agents[:, 2] + self.AGENT_FOV
+        rad_left = self.agents[:, 2] + self.agent_fov
         val = self.calc_rotation(rad_left)
         cond = val > trail_sum
         trail_sum = np.where(cond, val, trail_sum)
         result = np.where(cond, rad_left, result)
 
-        rad_right = self.agents[:, 2] - self.AGENT_FOV
+        rad_right = self.agents[:, 2] - self.agent_fov
         val = self.calc_rotation(rad_right)
         cond = val > trail_sum
         trail_sum = np.where(cond, val, trail_sum)
@@ -107,16 +107,16 @@ class Simulation:
 
         self.agents[:, 2] = result
 
-        cond = np.random.rand(self.AGENTS_NUM) < .1
+        cond = np.random.rand(self.agents_num) < .1
         self.agents[cond, 2] += np.where(np.random.randint(
-            2, size=cond.sum()), self.AGENT_FOV, -self.AGENT_FOV)
+            2, size=cond.sum()), self.agent_fov, -self.agent_fov)
 
     def calc_rotation(self, rad: np.ndarray[any, np.floating]) -> np.ndarray[any, np.int32]:
         h, w = self.agents[:, :2].astype(int).T
-        h_v = np.clip(h + self.VISION_DISTANCE * np.sin(rad),
-                      0, self.MAP_SIZE - 1).astype(int)
-        w_v = np.clip(w + self.VISION_DISTANCE * np.cos(rad),
-                      0, self.MAP_SIZE - 1).astype(int)
+        h_v = np.clip(h + self.vision_distance * np.sin(rad),
+                      0, self.map_size - 1).astype(int)
+        w_v = np.clip(w + self.vision_distance * np.cos(rad),
+                      0, self.map_size - 1).astype(int)
 
         return np.vectorize(self.sum_trails)(h_v, w_v)
         # return self.trails_map[h_v, w_v]
@@ -129,30 +129,30 @@ class Simulation:
         self.trails_map[h, w] = 255
 
     def dissipate_trails(self):
-        self.new_trails = np.zeros((self.MAP_SIZE, self.MAP_SIZE))
+        self.new_trails = np.zeros((self.map_size, self.map_size))
 
-        for h in range(self.MAP_SIZE):
-            for w in range(self.MAP_SIZE):
+        for h in range(self.map_size):
+            for w in range(self.map_size):
                 blur_value = self.trails_map[
-                    max(h-1, 0):min(h+1, self.MAP_SIZE-1),
-                    max(w-1, 0):min(w+1, self.MAP_SIZE-1)].sum() 
+                    max(h-1, 0):min(h+1, self.map_size-1),
+                    max(w-1, 0):min(w+1, self.map_size-1)].sum() 
 
                 # lerp between current value and blurred value
                 self.new_trails[h, w] = self.trails_map[h, w] * \
                     (1 - self.DISSIPATION) + blur_value * self.DISSIPATION
 
-                self.new_trails[h, w]=max(0, blur_value - self.DISSIPATION_RATE)
+                self.new_trails[h, w]=max(0, blur_value - self.dissipation_rate)
 
         self.trails_map = self.new_trails
         # self.trails_map = np.vectorize(self.blur_trails)(self.trails_map) * .97
 
-        # for h in range(self.MAP_SIZE):
-        #     for w in range(self.MAP_SIZE):
+        # for h in range(self.map_size):
+        #     for w in range(self.map_size):
         #         self.trails_map[h, w] = max(0, self.trails_map[h, w] - self.DISSIPATION)
 
-        # if self.tick % self.BLUR_DELAY:
+        # if self.tick % self.blur_delay:
         #     self.trails_map = cv.boxFilter(
-        #         self.trails_map,-1, (self.BLUR_STRENTH, self.BLUR_STRENTH)
+        #         self.trails_map,-1, (self.blur_strenth, self.blur_strenth)
         #     )
 
         # self.trails_map *= .97
@@ -163,13 +163,13 @@ class Simulation:
             x = w + i
             for j in range(-1, 2):
                 y = h + j
-                if 0 <= x < self.MAP_SIZE and 0 <= y < self.MAP_SIZE:
+                if 0 <= x < self.map_size and 0 <= y < self.map_size:
                     trail_sum += self.trails_map[y, x]
 
         return trail_sum/9
 
     def draw_map(self) -> None:
-        self.frame = np.zeros((self.MAP_SIZE, self.MAP_SIZE)) + self.trails_map
+        self.frame = np.zeros((self.map_size, self.map_size)) + self.trails_map
 
 
 if "__main__" == __name__:
